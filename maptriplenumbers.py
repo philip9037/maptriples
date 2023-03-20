b9087f6d599884e4949f3f2e2d45d5a8f2fb99d8
philip9037 <124433682+philip9037@users.noreply.github.com>
2023-03-20 19:38:21 +0530

    Add files via upload

---
+++ maptriplenumbers.py
@@ -0,0 +1,11 @@
+#sample list: [1, 2, 3, 4, 5, 6, 7]
+#Triple of list numbers:
+#[3, 6, 9, 12, 15, 18, 21]
+
+numbers =  [1, 2, 3, 4, 5, 6, 7]
+triples = list(map(lambda x:x*3,numbers))
+print(triples)
+
+
+
+
